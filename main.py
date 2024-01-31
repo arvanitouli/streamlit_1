62367ed9cbea623a2647a6edd0093ffc9efa30e0
arvanitouli <132561430+arvanitouli@users.noreply.github.com>
2024-01-31 23:13:06 +0200

    Ora 11
    First repository

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+number = st.number_input('Insert a number')
+st.write('The current number is ', number)
+
+number_1 = st.number_input('Insert second number')
+st.write('The current number is ', number_1)
+
+shumzimi = number * number_1
+st.write("Vlera e shumezuar", shumzimi)
+
